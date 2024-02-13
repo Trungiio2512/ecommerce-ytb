d6264290d7d839bc370a383cff8e4b174713b47f
trungiio <midiio2512@gmail.com>
2024-02-13 16:32:29 +0700

    refactor: handle cors

--- index.js
+++ index.js
@@ -5,21 +5,15 @@ const cookieParser = require("cookie-parser");
 const dbConnect = require("./config/connect");
 const initRoutes = require("./routes");
 const cors = require("cors");
-const port = process.env.PORT || 8888;
+const corsOptions = require("./config/cors");
 
 const app = express();
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: process.env.URL_CLIENT,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    credentials: true,
-  }),
-);
 // app.use(bodyParser.json());
 
 dbConnect();
 initRoutes(app);
-app.listen(port, () => console.log("port is running on port " + port));
+app.listen(process.env.PORT, () => console.log("port is running on port " + port));
